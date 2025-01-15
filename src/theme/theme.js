e21f89615a7d56d70f05a8b32129867f1d4da742
Andrii Rudiuk <dron74961@gmail.com>
2025-01-15 10:15:26 +0200

    fixes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -30,16 +30,6 @@ const baseTheme = {
 
 const palettes = {
   light: {
-    // veryDarkGray: '#121214',
-    // darkGray: '#181819',
-    // darkSlate: '#24262A',
-    // charcoalGray: '#19191C',
-    // mediumGray: '#6B7280',
-    // lightGray: '#9CA3AF',
-    // offWhiteGray: '#D1D5DB',
-    // lightOffWhite: '#E5E7EB',
-    // veryLightGray: '#F3F4F6',
-    // nearWhite: '#F9FAFB',
     background: {
       default: '#F3F4F6',
       list: '#FFFFFF'
@@ -65,16 +55,6 @@ const palettes = {
     border: {
       list:'#121214'
     }
-    // deepRed: '#881337',
-    // darkPink: '#9F1239',
-    // richRed: '#BE123C',
-    // crimson: '#E11D48',
-    // hotPink: '#F43F5E',
-    // lightPink: '#FB7185',
-    // palePink: '#FDA4AF',
-    // softPink: '#FECDD3',
-    // lightBlush: '#FFE4E6',
-    // lightGunmetal: '#FFF1F2'
   }
 }
 
