ddffd9b077d67c510d9ab04c61438b4659c98c1f
Andrii Rudiuk <dron74961@gmail.com>
2025-01-16 09:47:44 +0200

    fixes

--- src/router/router.js
+++ src/router/router.js
@@ -2,13 +2,15 @@ import { createBrowserRouter } from 'react-router-dom'
 import { MainLayout } from 'layouts/mainLayout/mainLayout'
 import { MainPage } from 'pages/mainPage/mainPage'
 
-export const router = createBrowserRouter([
-  {
-    element: <MainLayout />,
-    path: '/',
-    children: [
-      { path: '/', element: <MainPage /> }
-    ]
-  },
+export const router = createBrowserRouter(
+  [
+    {
+      element: <MainLayout />,
+      path: '/',
+      children: [
+        { path: '/', element: <MainPage /> }
+      ]
+    }
+  ],
   { basename: '/playlist' }
-])
+)
